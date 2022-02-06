d4fdd07f817f7466e6ac76028d01ac2756d9cf41
unknown <mitrehealthcare@gmail.com>
2022-02-05 22:40:02 -0800

    buttons actualization

--- assets/script.js
+++ assets/script.js
@@ -3,10 +3,7 @@ console.log('JavaScript code has loaded!')
 // First, tell us your name
 let yourName = "Adrian Mitre" // HINT: Replace this with your own name!
 
-// We'll use these variables to track the counts of each cookie type
-let gb = 0 // Gingerbread
-let cc = 0 // Chocolate Chip
-let sugar = 0 // Sugar Sprinkle
+
 
 // selecting the element with an id of credit
 const credit = document.querySelector('#credit')
@@ -15,51 +12,117 @@ const credit = document.querySelector('#credit')
 // Code to update name display
 credit.textContent = `Created by ${'ADRIAN MITRE'}`
 
-// update quantity    script.js
-function updateQuantity(displayQuantity) {
-    let quantity = document.querySelector('.total-quantity')
-    quantity.innerHTML = displayQuantity
 
-// selecting the element with an id of gb minus:buttons
-const gbMinusBtn = document.querySelector('#minus-gb')
-gbMinusBtn.addEventListener('click', function() {
-    if (gb = 0) {
-        gb--
-    }
-    updateQuantity(`qty-gb: ${gb}`)
-})
- 
-/// selecting the element with an id of gb plus:buttons
+
+// update quantity gb button
+let gb = 0
+let total = 0
+
+function updateGb(displayTotal){
+  let quantity = document.querySelector('#qty-gb')
+  quantity.innerHTML = displayTotal
+} 
+
 const gbPlusBtn = document.querySelector('#add-gb')
+const gbMinusBtn = document.querySelector('#minus-gb')
 gbPlusBtn.addEventListener('click', function() {
+  gb++
+  total++
+  updateTotal(`${total}`)
+  updateGb(`${gb}`)
 
+})
+gbMinusBtn.addEventListener('click', function() {
+  if (gb > 0) {
+  gb--
+  total--
+  }
+updateTotal(`${total}`)
+updateGb(`${gb}`)
 
 })
 
-// selecting the element with an id of cc minus:buttons
-const ccMinusBtn = document.querySelector('#minus-cc')
-ccMinusBtn.addEventListener('click', function() {
 
-})
+// update quantity cc button
+let cc = 0
 
-// selecting the element with an id of cc plus:buttons
+function updateCc(displayTotal){
+  let quantity = document.querySelector('#qty-cc')
+  quantity.innerHTML = displayTotal
+} 
+
 const ccPlusBtn = document.querySelector('#add-cc')
+const ccMinusBtn = document.querySelector('#minus-cc')
 ccPlusBtn.addEventListener('click', function() {
+  cc++
+  total++
+  updateTotal = (`${total}`)
+  updateCc = (`${cc}`)
+
+})
+ccMinusBtn.addEventListener('click', function() {
+  if (cc > 0) {
+  cc--
+  total--
+  }
+updateTotal = (`${total}`)
+updateCc = (`${cc}`)
 
 })
 
-// selecting the element with an id of sugar minus:buttons
-const sugarMinusBtn = document.querySelector('#minus-sugar')
-sugarMinusBtn.addEventListener('click', function() {
+
+// update quantity Sugar button
+let sugar = 0
+
+function updateSugar(displayTotal){
+  let quantity = document.querySelector('#qty-sugar')
+  quantity.innerHTML = displayTotal
+} 
+
+const SugarPlusBtn = document.querySelector('#add-sugar')
+const SugarMinusBtn = document.querySelector('#minus-sugar')
+SugarPlusBtn.addEventListener('click', function() {
+  sugar++
+  total++
+  updateTotal = (`${total}`)
+  updateSugar = (`${sugar}`)
+
+})
+SugarMinusBtn.addEventListener('click', function() {
+  if (sugar > 0) {
+  sugar--
+  total--
+  }
+updateTotal = (`${total}`)
+updateSugar = (`${sugar}`)
 
 })
 
-// selecting the element with an id of sugar plus:buttons
-const sugarPlusBtn = document.querySelector('#add-sugar')
-sugarPlusBtn.addEventListener('click', function() {
+
+// update quantity Total button
+let sugar = 0
+
+function updateTotal(displayTotal){
+  let quantity = document.querySelector('#qty-total')
+  quantity.innerHTML = displayTotal
+} 
+
+const TotalPlusBtn = document.querySelector('#add-total')
+const TotalMinusBtn = document.querySelector('#minus-total')
+TotalPlusBtn.addEventListener('click', function() {
+  total++
+  total++
+  updateTotal = (`${total}`)
+  updateTotal = (`${total}`)
+
+})
+SugarMinusBtn.addEventListener('click', function() {
+  if (total > 0) {
+  total--
+  total--
+  }
+updateTotal = (`${total}`)
+updateSugar = (`${total}`)
 
 })
 
-
-
-}
